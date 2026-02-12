7fb1b68313b250c1d2ffc3db5516fdfd6e1c3bac
Jack McCall <jackson.mccall907@gmail.com>
2026-02-12 16:45:34 -0600

    Add project files.

---
+++ wakuwaku.cpp
@@ -0,0 +1,12 @@
+﻿// wakuwaku.cpp : Defines the entry point for the application.
+//
+
+#include "wakuwaku.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
